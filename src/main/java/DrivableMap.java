b65e10c4e7ef0fb42265a08e708dbaef734c3185
Luke Krikorian <lukekrikorian@gmail.com>
2022-10-07 15:28:54 -0400

    Implement missing methods

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -28,7 +28,14 @@ class DrivableMap {
      *       Return true if the Drivable was added to drivable_map.
      */
 
+	public boolean addDrivable(String id, Drivable object) {
+		if (drivable_map.containsKey(id)) {
+			return false;
+		}
 
+		drivable_map.put(id, object);
+		return true;
+	}
 
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
@@ -38,16 +45,30 @@ class DrivableMap {
      * iterate through drivable_map.
      */
 
+	public boolean hasFasterThan(int speed) {
+		for (Drivable obj : drivable_map.values()) {
+			if (obj.getMaxSpeed() >= speed) {
+				return true;
+			}
+		}
 
-
-
+		return false;
+	}
 
     /* TODO: Write a method named getTradable that takes no arguments and
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
 
+	public List<Tradable> getTradable() {
+		List<Tradable> list = new ArrayList<Tradable>();
+		
+		for (Drivable obj : drivable_map.values()) {
+			if (obj instanceof Tradable) {
+				list.add((Tradable) obj);
+			}
+		}
 
-
-    
+		return list;
+	}
 }
